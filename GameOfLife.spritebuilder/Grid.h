46c29c9a501328d3518cdb73e3e5f860ee63dbfc
Nina Baculinao <nb2406@columbia.edu>
2014-06-24 11:23:32 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,16 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Nina Baculinao on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive; // will be used to store current game stats for display
+@property (nonatomic, assign) int generation;
+
+@end
